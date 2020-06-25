8d3b9d67240b12b451552fe85c9ba1752de33514
Sam <hackoor83@gmail.com>
2020-06-25 09:58:23 +0200

    first commit

---
+++ src/main/java/com/example/citesting/CitestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.citesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/citesting/CitestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
